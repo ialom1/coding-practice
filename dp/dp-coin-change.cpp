a98c1e682377e69f66ccb9391929613d0145e57d
shakib <ifteher.alom@gmail.com>
2020-01-04 01:27:56 +0600

    coin change

---
+++ dp/dp-coin-change.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int countChangeWays(int arr[], int m, int n){
+  int dp[n+1] = {0};
+  dp[0] = 1;
+  for(int i=0; i<m; i++){
+    int coin = arr[i];
+    for(int j=coin; j<=n; j++){
+      dp[j] = dp[j] + dp[j-coin];
+    }
+  }
+  return dp[n];
+}
+
+int main() {
+  int arr[] = {2, 3, 5, 6};
+  int m = sizeof(arr)/sizeof(arr[0]);
+  int n = 10;
+  std::cout << countChangeWays(arr, m, n) << '\n';
+  return 0;
+}
